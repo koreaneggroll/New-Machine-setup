729581440c0fb393b1f122d07f745f02aa21669d
Arin <72822356+koreaneggroll@users.noreply.github.com>
2021-02-02 15:25:04 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install build-essential
+snap install brave
+sudo add-apt-repository ppa:agornostal/ulauncher
+sudo apt install ulauncher
+sudo apt install yakuake
+sudo apt install vim
+sudo apt install vlc
+sudo apt install steam
